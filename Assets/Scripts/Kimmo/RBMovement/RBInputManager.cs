c19df824d5ebe6715f1ed7d0cd6623db8ff44d84
Kimmo Riipinen <riipinenkimmo@gmail.com>
2024-09-17 14:29:38 +0300

    Fixed URP and started to work with rigidbody movement

---
+++ Assets/Scripts/Kimmo/RBMovement/RBInputManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RBInputManager : MonoBehaviour
+{
+    [SerializeField] RBPlayerMovement playerMovement;
+    public AapoSwordSwing aapoSwordSwing;
+    [SerializeField] MouseLook mouseLook;
+
+    PlayerControls controls;
+    PlayerControls.MovementActions movement;
+
+    Vector2 horizontalInput;
+    Vector2 mouseInput;
+
+    bool isMoving;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        movement = controls.Movement;
+        
+        movement.HorizontalMovement.performed += ctx => horizontalInput = ctx.ReadValue<Vector2>();
+        movement.HorizontalMovement.performed += HorizontalInputCheck;
+
+        movement.Jump.performed += ctx => playerMovement.OnJumpPressed();
+
+        movement.MouseX.performed += ctx => mouseInput.x = ctx.ReadValue<float>();
+        movement.MouseY.performed += ctx => mouseInput.y = ctx.ReadValue<float>();
+
+        movement.Dash.performed += ctx => playerMovement.OnDashPressed();
+
+        movement.Slide.performed += ctx => playerMovement.OnSlidePressed();
+
+        movement.Attack.performed += ctx => aapoSwordSwing.SwingSword();
+    }
+
+    private void Update()
+    {
+        playerMovement.ReceiveInput(horizontalInput);
+        mouseLook.ReceiveInput(mouseInput);
+    }
+
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        controls.Disable();
+    }
+
+    private void HorizontalInputCheck(InputAction.CallbackContext ctx)
+    {
+        Vector2 movementInput = ctx.ReadValue < Vector2>();
+        if (movementInput == Vector2.zero)
+        {
+            playerMovement.isMoving = false;
+
+        }
+        else 
+        {
+            playerMovement.isMoving = true;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Kimmo/RBMovement/RBPlayerMovement.cs
@@ -0,0 +1,178 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RBPlayerMovement : MonoBehaviour
+{
+    // WASD Movement
+    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float gravity;
+    [SerializeField] float jumpHeight;
+    [SerializeField] LayerMask groundMask;
+    bool canMove;
+    public bool isMoving;
+    Vector2 horizontalInput;
+    
+    // Jump
+    Vector3 verticalVelocity = Vector3.zero;
+    public bool isGrounded;
+    bool isJumping;
+
+    // Dash
+    public Transform orientation;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashTime;
+    [SerializeField] float dashCooldown;
+    float dashCooldownTimer;
+
+    // Slide
+    [SerializeField] float slideSpeed;
+    [SerializeField] float slideTime;
+    float slideCooldownTimer;
+    [SerializeField] bool canSlide;
+    [SerializeField] Transform cam;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.freezeRotation = true;
+
+        canMove = true;
+        canSlide = true;
+    }
+
+    private void Update()
+    {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.1f, groundMask);
+
+        if (isGrounded)
+        {
+            verticalVelocity.y = 0;
+        }
+
+        if (canMove)
+        {
+            Vector3 horizontalVelocity = (transform.right * horizontalInput.x + transform.forward * horizontalInput.y) * moveSpeed;
+            rigidbody.AddForce(horizontalVelocity * Time.deltaTime);
+            //controller.Move(horizontalVelocity * Time.deltaTime);
+        }
+
+        if (isJumping)
+        {
+            if (isGrounded)
+            {
+                verticalVelocity.y = Mathf.Sqrt(-2f * jumpHeight * gravity);
+            }
+
+            isJumping = false;
+        }
+
+        verticalVelocity.y += gravity * Time.deltaTime;
+        //controller.Move(verticalVelocity * Time.deltaTime);
+
+        if (dashCooldownTimer > 0)
+        {
+            dashCooldownTimer -= Time.deltaTime;
+        }
+    }
+
+    public void ReceiveInput(Vector2 _horizontalInput)
+    {
+        horizontalInput = _horizontalInput;
+    }
+
+    public void OnJumpPressed()
+    {
+        isJumping = true;
+    }
+
+    public void OnDashPressed()
+    {
+        if (dashCooldownTimer > 0) return;
+        else dashCooldownTimer = dashCooldown;
+
+        canMove = false;
+        StartCoroutine(Dash());
+        canMove = true;
+    }
+
+    private Vector3 GetDirection(Transform forwardT)
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3();
+        direction = forwardT.forward * verticalInput + forwardT.right * horizontalInput;
+
+        if (verticalInput == 0 && horizontalInput == 0)
+        {
+            direction = forwardT.forward;
+        }
+
+        return direction.normalized;
+    }
+
+    IEnumerator Dash()
+    {
+        float startTime = Time.time;
+
+        Transform forwardT;
+        forwardT = orientation;
+
+        Vector3 direction = GetDirection(forwardT);
+
+        while (Time.time < startTime + dashTime)
+        {
+            Vector3 forceToApply = direction * dashSpeed;
+            //controller.Move(forceToApply * Time.deltaTime);
+            
+            yield return null;
+        }
+    }
+
+    public void OnSlidePressed()
+    {
+        if (canSlide && isMoving)
+        {
+            canMove = false;
+            //controller.height = 1f;
+            //controller.center = new Vector3(0,-0.5f,0);
+            cam.localPosition = new Vector3(0, 0, 0.5f);
+            StartCoroutine(Slide());
+        }
+    }
+
+    IEnumerator Slide()
+    {
+        float startTime = Time.time;
+
+        slideSpeed = moveSpeed * 1.5f;
+
+        while (Time.time < startTime + slideTime)
+        {
+            canSlide = false;
+            Vector3 forceToApply = orientation.forward * slideSpeed;
+            //controller.Move(forceToApply * Time.deltaTime);
+
+            if (slideSpeed > moveSpeed)
+            {
+                slideSpeed -= Time.deltaTime * slideTime;
+            }
+            yield return null;
+        }
+        OnCoroutineStopped();
+    }
+
+    private void OnCoroutineStopped()
+    {
+        canMove = true;
+        canSlide = true;
+        slideSpeed = moveSpeed;
+        //controller.height = 2f;
+        //controller.center = Vector3.zero;
+        cam.localPosition = new Vector3(0, 0.5f, 0.5f);
+    }
+}
