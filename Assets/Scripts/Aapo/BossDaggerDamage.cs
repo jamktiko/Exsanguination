4e4bc7b40d68feed7019e04a778cd0dedc43954c
Aapo Vanhainen <89449819+ab0136@users.noreply.github.com>
2024-11-07 14:47:57 +0200

    added boss dagger script to be attached into daggers, they can damage player and be reflected back

---
+++ Assets/Scripts/Aapo/BossDaggerDamage.cs
@@ -0,0 +1,77 @@
+using EmiliaScripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDaggerDamage : MonoBehaviour
+{
+
+    [SerializeField] int damage;
+    [SerializeField] float projectileSpeed;
+    private PlayerCombat playerCombat;
+    private PlayerHealthManager playerHealthManager;
+    private Transform player;             // Reference to the player's position
+    private Rigidbody rb;
+    public bool isReflected;
+
+    private void Awake()
+    {
+        playerCombat = GameObject.FindGameObjectWithTag("PlayerModel").GetComponent<PlayerCombat>();
+        playerHealthManager = GameObject.FindGameObjectWithTag("HealthManager").GetComponent<PlayerHealthManager>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            if (playerCombat.isBlocking)
+            {
+                rb.isKinematic = true;
+                Vector3 directionToEnemy = (transform.position - player.position).normalized;
+                rb.isKinematic = false;
+                rb.velocity = directionToEnemy * projectileSpeed;  // Set the velocity directly
+                isReflected = true;
+                Debug.Log("reflected");
+            }
+            else
+            {
+                playerHealthManager.UpdatePlayerHealth(-damage);
+                Debug.Log("Hit player");
+            }
+
+        }
+
+        else if (isReflected && collision.collider.CompareTag("Boss"))
+        {
+            EnemyHealthScript enemyHealthScript = collision.collider.GetComponent<EnemyHealthScript>();
+
+            if (enemyHealthScript != null)
+            {
+                enemyHealthScript.ChangeEnemyHealth(damage);
+                Debug.Log("Reflected back to enemy and dealt damage");
+            }
+            else
+            {
+                Debug.LogWarning("EnemyHealthScript not found on " + collision.collider.name);
+            }
+        }
+
+        else
+
+        {
+            ResetValues();
+        }
+    }
+
+    private void ResetValues()
+    {
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        gameObject.SetActive(false);
+        isReflected = false;
+
+    }
+}
