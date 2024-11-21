8b00ca5ffde189ab19fc4cc074b3134d2e76be13
Aapo Vanhainen <89449819+ab0136@users.noreply.github.com>
2024-11-21 14:29:08 +0200

    new health

--- Assets/GothicUI/Scripts/HPFlowController.cs
+++ Assets/GothicUI/Scripts/HPFlowController.cs
@@ -1,20 +1,45 @@
-﻿using UnityEngine;
+﻿using EmiliaScripts;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace CrusaderUI.Scripts
 {
-	public class HPFlowController : MonoBehaviour {
-	
-		private Material _material;
+    public class HPFlowController : MonoBehaviour
+    {
+        private Material _material;
+        private PlayerHealthManager playerHealthManager;
 
-		private void Start ()
-		{
-			_material = GetComponent<Image>().material;
-		}
+        private void Awake()
+        {
+            playerHealthManager = GameObject.FindGameObjectWithTag("HealthManager").GetComponent<PlayerHealthManager>();
+        }
 
-		public void SetValue(float value)
-		{
-			_material.SetFloat("_FillLevel", value);
-		}
-	}
+        private void Start()
+        {
+            _material = GetComponent<Image>().material;
+            _material.SetFloat("_FillLevel", 1);
+        }
+
+        public void SetValue(float value)
+        {
+            _material.SetFloat("_FillLevel", value);
+        }
+
+        private void OnEnable()
+        {
+            playerHealthManager.OnHealthUpdate += OnPlayerHealthUpdate;
+        }
+
+        private void OnDisable()
+        {
+            playerHealthManager.OnHealthUpdate -= OnPlayerHealthUpdate;
+        }
+
+        private void OnPlayerHealthUpdate()
+        {
+            // Calculate the health percentage and update the material's fill level
+            float healthPercentage = (float)playerHealthManager.CurrentPlayerHealth() / playerHealthManager.MaxPlayerHealth();
+            SetValue(healthPercentage);
+        }
+    }
 }
