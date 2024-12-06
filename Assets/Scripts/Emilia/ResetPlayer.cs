40cc529abac24dff6989541fa3fefd9f01b7dc52
Aapo Vanhainen <89449819+ab0136@users.noreply.github.com>
2024-12-06 14:56:35 +0200

    added new spawnpoint for jump room

--- Assets/Scripts/Emilia/ResetPlayer.cs
+++ Assets/Scripts/Emilia/ResetPlayer.cs
@@ -9,6 +9,7 @@ public class ResetPlayer : MonoBehaviour
     public bool hasTriggered;
     public bool isTutorial;
     public bool isGrappleRoom;
+    public bool isCharlesHyppyHuone;
 
     // Start is called before the first frame update
     void Awake()
@@ -21,6 +22,13 @@ public class ResetPlayer : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             if (isGrappleRoom)
+            {
+                player.transform.SetPositionAndRotation(resetSpot[1].position, Quaternion.identity);
+                player.transform.rotation = Quaternion.Euler(0, 180, 0);
+                Debug.Log("grapple drop");
+            }
+
+            if (isCharlesHyppyHuone)
             {
                 player.transform.SetPositionAndRotation(resetSpot[2].position, Quaternion.identity);
                 player.transform.rotation = Quaternion.Euler(0, 180, 0);
@@ -40,12 +48,7 @@ public class ResetPlayer : MonoBehaviour
 
                 }
 
-                else
-                {
-                    player.transform.SetPositionAndRotation(resetSpot[1].position, Quaternion.identity);
-                    player.transform.rotation = Quaternion.Euler(0, -90f, 0);
-
-                }
+               
 
             }
            
